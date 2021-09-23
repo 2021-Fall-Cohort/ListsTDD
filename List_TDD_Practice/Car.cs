4334126953dfb8d7c4c961aff434bc66828efcc0
Carlos Lopez <carlos@wecancodeit.org>
2021-09-23 10:27:05 -0400

    Added Timer Example for Fake Car Class

---
+++ List_TDD_Practice/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace List_TDD_Practice
+{
+    public class Car
+    {
+        public int Fuel = 100;
+
+        public void Drive()
+        {
+            Fuel -= 10;
+        }
+    }
+
+    
+}
--- List_TDD_Practice/Program.cs
+++ List_TDD_Practice/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace List_TDD_Practice
 {
     class Program
     {
+        public static List<Car> cars = new List<Car>();
+        public static Car car = new Car();
+        public static Timer _timer;
         static void Main(string[] args)
         {
             Console.WriteLine("Let's practice creating tests for Lists!");
+
+            _timer = new Timer(MyMethod, null, 0, 15000);
+
+            cars.Add(new Car());
+
+            Console.Read();
+        }
+
+        public static void MyMethod(Object o)
+        {
+            //Console.WriteLine("I should print out every 2 seconds...");
+            //car.Drive();
+            foreach(var c in cars)
+            {
+                c.Drive();
+            }
         }
     }
 }
